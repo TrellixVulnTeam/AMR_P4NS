57b6c5f5dfdb5f411152cb213d22a5bad61bc314
Adriano Zaghi - adriano.zaghi@studio.unibo.it <STUDENTI\adriano.zaghi@str957-bio3.personale.dir.unibo.it>
2022-08-10 12:52:17 +0200

    Added comments to M_learning.py

--- M_learning.py
+++ M_learning.py
@@ -11,18 +11,41 @@ import json
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import statistics
 from os.path import isfile, join
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_validate
 from sklearn.model_selection import LeaveOneOut
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import train_test_split
 
 average_read_length = 170
 
+def read_kmer(kmer_dir):
+	"""This function reads the output of kmc_count
+	The rest of the analysis is not already supported for this kind of data
+	"""
+	files = [f for f in os.listdir(kmer_dir) if isfile(join(kmer_dir, f))]
+	kmer_collection_of_samples = {}
+	for F in files:
+		with open(join(kmer_dir, F), mode = "r") as f:
+			kmer_collection_of_samples = pd.read_csv(f, sep='\t')
+	return kmer_collection_of_samples
+	
+
 def read_bwt_2(bwt_dir):
+	"""Read the output of rgi
+	Paramethers:
+		bwt_dir: the path to the directory where the files to be read are stored
+	Returns:
+		a two dimensional dictionary: at first level we have a sequence of sample_name:resistome
+		each resistome have the structure ARO_identity:FPKB_estimated_abundance
+	"""
 	files = [f for f in os.listdir(bwt_dir) if isfile(join(bwt_dir, f))]
 	resistomes = {}
 	for F in files:
@@ -40,6 +63,17 @@ def read_bwt_2(bwt_dir):
 	return resistomes
 
 def read_metadata(meta_dir, key):
+	"""Read the metadata file
+	Paramethers:
+		meta_dir:
+			The path to the directory where the metadata are stored.
+			Notice that each metadata file have to be named exaccly equal to the data file (from rgi_bwt or kmc_count) 
+		key:
+			This key is refered to the structure of the json file used as metadata.
+			Specify which value to consider as a label in the machine learning procedure that will follow, "phenotype" in our case
+	Returns:
+		A dictionary with the structure ARO_identity: phenotype
+	"""
 	files = [f for f in os.listdir(meta_dir) if isfile(join(meta_dir, f))]
 	metadata = {}
 	for F in files:
@@ -49,6 +83,11 @@ def read_metadata(meta_dir, key):
 	return metadata
 
 def filter_sparcity_median(dataset, sparsity_filter, median_filter, plot = False, plot_zoom = 1):
+	"""Perform the filter operations on the database
+	The values for the median and the sparsity filters are the two positional arguments, while the standard arguments regulates the plots of the function
+	See my thesis for a bether explenation of the effects of those filters
+	The original dataset is not modified.
+	"""
 	new = dataset.copy()
 	if plot:
 		y = dataset.drop("phenotype", axis=1).median(axis=0)
@@ -64,7 +103,7 @@ def filter_sparcity_median(dataset, sparsity_filter, median_filter, plot = False
 		plt.title("Filtering results")
 		plt.plot([0,100],[median_filter,median_filter], color = "red") #median
 		plt.plot([sparsity_filter,sparsity_filter],[0,1500], color = "red") #sparsity
-		plt.show()
+		plt.show()		
 	for (c_name, c_data) in new.drop("phenotype", axis=1).iteritems():
 		zeros_percent = (c_data == 0).astype(int).sum()*100/new.shape[0]
 		if zeros_percent > sparsity_filter:
@@ -78,10 +117,23 @@ def filter_sparcity_median(dataset, sparsity_filter, median_filter, plot = False
 	aro_left_out = [k for k in dataset.drop("phenotype", axis = 1).columns if k not in new.columns]
 	left_out = dataset[aro_left_out]
 	print("È stato rimosso " + str(left_out.to_numpy().sum() / dataset.drop("phenotype", axis = 1).to_numpy().sum()) + " del contenuto dei resistomi")
-	#FAI GRAFICO PER UN EQUA DISTRIBUZIONE DELLA PERDITA DI INFORMAZIONE
+	X = list(dataset.index)
+	Yprima = list(dataset.drop("phenotype", axis=1).sum(axis = 1))
+	Ydopo = list(new.drop("phenotype", axis=1).sum(axis = 1))
+	X_axis = np.arange(len(X))
+	plt.bar(X_axis - 0.2, Yprima, 0.4, label = 'Before')
+	plt.bar(X_axis + 0.2, Ydopo, 0.4, label = 'After')
+	plt.xticks(X_axis, X)
+	plt.xlabel("Populations")
+	plt.ylabel("Spectrum integral for the population")
+	plt.legend()
+	plt.show()	
 	return new
 
 def view_database(dataset, view_a_spectrum = None):
+	"""Produces various plots of the dataset provided as argument and also of a specific sample
+	if "view_a_spectrum" have a reasonable integer value
+	"""
 	V = dataset.drop("phenotype", axis = 1).sum(axis = 1)
 	dic = {a : V[a] for a in V.index.tolist()}
 	dic = dict(sorted(dic.items(), key=lambda item: item[1]))
@@ -101,6 +153,9 @@ def view_database(dataset, view_a_spectrum = None):
 		plt.show()
 		
 def CLR_normalization(dataset):
+	"""Performs the CLR normalization on the dataset in argument
+	The original dataset is not modified	
+	"""
 	temporary = dataset.replace(to_replace = 0, value = 0.5)
 	temporary = temporary.drop("phenotype", axis = 1)
 	ph = dataset["phenotype"].tolist()
@@ -110,9 +165,15 @@ def CLR_normalization(dataset):
 	return temporary
 
 def PCA_maker(dataset, numero_componenti = None, plots = False):
+	"""Performs the PCA on the dataset
+	It can be set a maximal number of components to be kept, otherwise we will end up with a number of them equal to the number of samples
+	If plot is set thrue the function produces also some usefull plots
+	The original dataset is not modified
+	"""
 	pca = PCA()	
 	pca.fit(dataset.drop("phenotype", axis = 1).T)
 	D = pd.DataFrame(pca.components_, columns=dataset.index.values.tolist()).T
+	D.columns = ["component "+str(i) for i in range(D.shape[1])]
 	if plots:
 		V = pd.DataFrame(pca.explained_variance_ratio_)
 		a = [str(v) for v in list(V.index)]
@@ -120,7 +181,7 @@ def PCA_maker(dataset, numero_componenti = None, plots = False):
 		plt.bar(a,b)
 		plt.xlabel("PCA components")
 		plt.ylabel("Explained variance rateo")
-		plt.show()
+		plt.show()	
 		plt.scatter(D.iloc[:,0], D.iloc[:,1], color = ["red" if pheno == "resistant" else "blue" for pheno in list(dataset["phenotype"])])
 		plt.xlabel("First component")
 		plt.ylabel("Second component")
@@ -129,6 +190,9 @@ def PCA_maker(dataset, numero_componenti = None, plots = False):
 	return D
 	
 def std_scale(dataset):
+	"""Performs the standard scaling of the valueds in the dataframe
+	The original dataset is not modified
+	"""
 	scale = StandardScaler()
 	nuovo = scale.fit_transform(dataset.drop("phenotype", axis = 1))
 	nuovo = pd.DataFrame(nuovo, index = list(dataset.index), columns = list(dataset.drop("phenotype", axis = 1).columns))
@@ -136,18 +200,30 @@ def std_scale(dataset):
 	return nuovo
 	
 
-def create_train_test(dict_di_dataframe, phenotypes, sparsity=70, median=5):
-#CREAZIONE DI UN PANDAS DATAFRAME NOME (CAMPIONE X GENE RESISTENTE) CON TUTTE LE ENTRATE NECESSARIE
+def create_train_test(dict_dict, phenotypes, sparsity=70, median=5):
+	"""Creates the database in a format usefull for machine learning
+	Paramethers:
+		dict_dict: a dictionary containing other dictionary, this argument is supposed to be the returning value from read_bwt
+		phenotypes: a dictionary continining the ground throoth about sample's labels, labelled with the sample names
+		The standard agruments are passed to the functino filter_median_sparsity, see the comment of it
+	Returns:
+		A pandas dataframe containing all the components of the data (samples) after various operations are performed: we have
+		-Filtering the columns for their sparsity and median vales
+		-Normalization with center log rateo
+		-Standard scaling
+		-PCA
+	Each of the steps of this function are independent and can be commented it is required not to have them
+	"""
 	ind=[]
 	ll = []
-	for a in dict_di_dataframe:
-		ind += list(dict_di_dataframe[a].index)
+	for a in dict_dict:
+		ind += list(dict_dict[a].index)
 	ind = list(set(ind))
-	for d in dict_di_dataframe:
+	for d in dict_dict:
 		new_data = {}
 		for i in ind:
-			if i in dict_di_dataframe[d].index:
-				new_data[i] = dict_di_dataframe[d].loc[i,"FPKB"]
+			if i in dict_dict[d].index:
+				new_data[i] = dict_dict[d].loc[i,"FPKB"]
 			else:
 				new_data[i] = 0
 		l = []
@@ -158,35 +234,111 @@ def create_train_test(dict_di_dataframe, phenotypes, sparsity=70, median=5):
 		ll.append(l)
 	dataset = pd.DataFrame(ll, columns=["population"]+ind+["phenotype"])
 	dataset.set_index("population", inplace=True)
-	view_database(dataset)
+#	view_database(dataset)
 #FILTERING FOR PREVALENCE #toglgie quelle con occorrenza di zeri maggiore del tot%
-	dataset = filter_sparcity_median(dataset, sparsity, median, plot = False, plot_zoom=15)
-	view_database(dataset, 2)
+	dataset = filter_sparcity_median(dataset, sparsity, median, plot = True, plot_zoom=15)
+#	view_database(dataset, 2)
 #NORMALIZE #normalizza con center og rateo
 	dataset = CLR_normalization(dataset)
 #MAKING STANDARDIZATION
 	dataset = std_scale(dataset)
 #MAKING PCA #il secondo argomento standard determina quante componenti tiene, in questo caso tutte
-	dataset = PCA_maker(dataset, plots=True)
+	dataset = PCA_maker(dataset, plots=False)
 	return dataset
 
-dati = read_bwt_2("bwt")
-phenotipi = read_metadata("metadata", "Phenotype")
+def loo(classifier, resistome, phenotype, plot = False, linear = False):
+	scores = cross_val_score(classifier, resistome, phenotype, cv=LeaveOneOut(), n_jobs=-1)
+	results = list(cross_val_predict(classifier, resistome, phenotype, cv=LeaveOneOut(), n_jobs=-1))
+	estimators = cross_validate(classifier, resistome, phenotype, cv=LeaveOneOut(), n_jobs=-1, return_estimator=True)
+	if linear:
+		feat_imps = [i.coef_[0] for i in estimators["estimator"]]
+	else:
+		feat_imps = [i.feature_importances_ for i in estimators["estimator"]]
+	feat_imps = pd.DataFrame(feat_imps, columns=resistome.columns)
+	feat_imps = {"Feature importance" : feat_imps.mean(axis=0),"Standard deviation": feat_imps.std(axis=0)}
+	if plot:
+		plt.hist(scores)
+		plt.xlabel("Scores")
+		plt.ylabel("Abundance")
+		plt.show()
+		X_, Y_ = [i.split(" ")[1] for i in feat_imps["Feature importance"].index], feat_imps["Feature importance"].values
+		if linear:
+			Y_ = [abs(iii) for iii in Y_]
+		plt.bar(X_, Y_)
+		plt.xlabel('PCA component')
+		plt.ylabel('Feature importance')
+		plt.errorbar(X_, Y_, feat_imps["Standard deviation"].values, fmt=' ',color='Black')
+		plt.show()
+	false_s , false_r = 0,0
+	for a in range(len(phenotype)):
+		if phenotype[a] == "resistant" and results[a] == "susceptible":
+			false_s +=1
+		elif phenotype[a] == "susceptible" and results[a] == "resistant":
+			false_r +=1
+	print("Leave one out report:")
+	print("False resistant = " + str(false_r))
+	print("False susceptible = " + str(false_s))
+	print("Scores = " + str(scores))
+	print("Score avrage " + str(sum(scores)/len(scores)) + ", variance " + str(np.var(scores)) + '\n')
+	return {"Scores":scores, "Feature importance" : feat_imps, "Falsi suscettibili":false_s, "Falsi resistenti":false_r}
+
+def cv5(classifier, resistome, phenotype, plot = False, linear = False):
+	scores = cross_val_score(classifier, resistome, phenotype, cv=5, n_jobs=-1)
+	results = list(cross_val_predict(classifier, resistome, phenotype, cv=5, n_jobs=-1))
+	estimators = cross_validate(classifier, resistome, phenotype, cv=5, n_jobs=-1, return_estimator=True)
+	if linear:
+		feat_imps = [i.coef_[0] for i in estimators["estimator"]]
+	else:
+		feat_imps = [i.feature_importances_ for i in estimators["estimator"]]
+	feat_imps = pd.DataFrame(feat_imps, columns=resistome.columns)
+	feat_imps = {"Feature importance" : feat_imps.mean(axis=0),"Standard deviation": feat_imps.std(axis=0)}
+	if plot:
+		plt.hist(scores)
+		plt.xlabel("Scores")
+		plt.ylabel("Abundance")
+		plt.show()
+		X_, Y_ = [i.split(" ")[1] for i in feat_imps["Feature importance"].index], feat_imps["Feature importance"].values
+		if linear:
+			Y_ = [abs(iii) for iii in Y_]
+		plt.bar(X_, Y_)
+		plt.xlabel('PCA component')
+		plt.ylabel('Feature importance')
+		plt.errorbar(X_, Y_, feat_imps["Standard deviation"].values, fmt=' ',color='Black')
+		plt.show()
+	false_s , false_r = 0,0
+	for a in range(len(phenotype)):
+		if phenotype[a] == "resistant" and results[a] == "susceptible":
+			false_s +=1
+		elif phenotype[a] == "susceptible" and results[a] == "resistant":
+			false_r +=1
+	print("5 fold report:")
+	print("False resistant = " + str(false_r))
+	print("False susceptible = " + str(false_s))
+	print("Scores = " + str(scores))
+	print("Score avrage " + str(sum(scores)/len(scores)) + ", variance " + str(np.var(scores)) + '\n')
+	return {"Scores":scores, "Feature importance" : feat_imps, "Falsi suscettibili":false_s, "Falsi resistenti":false_r}
+	
+
+dati = read_bwt_2("bwt_plot")
+#dati = read_bwt_2("bwt_4d")
+phenotipi = read_metadata("metadata_plot", "Phenotype")
+#phenotipi = read_metadata("metadata_4d", "Phenotype")
 Dati = create_train_test(dati, phenotipi)
 Resistome = Dati.drop("phenotype", axis=1)
 Phenotype = Dati["phenotype"]
-Ground_throot_bool = [1 if d == "resistant" else 0 for d in Dati["phenotype"]]
-Ground_throot = list(Dati["phenotype"])
+Phenotype_bool = pd.Series({k : (1 if Phenotype[k] == "resistant" else 0) for k in Phenotype.keys()})
 
-Forest = RandomForestClassifier()
-Net = ElasticNet(random_state=0)
-loo = LeaveOneOut()
-scores_loo_forest = cross_val_score(Forest, Resistome, Phenotype, cv=loo, n_jobs=-1)
-results_loo_forest = list(cross_val_predict(Forest, Resistome, Phenotype, cv=loo, n_jobs=-1))
-scores_cv5_forest = cross_val_score(Forest, Resistome, Phenotype, cv=5, n_jobs=-1)
-results_cv5_forest = list(cross_val_predict(Forest, Resistome, Phenotype, cv=5, n_jobs=-1))
+print("\nForest classifier:")
+Forest = RandomForestClassifier(random_state=0)
+Forest_loo_scores = loo(Forest, Resistome, Phenotype, plot=True)
+Forest_cv5_scores = cv5(Forest, Resistome, Phenotype, plot=True)
 
-#scores_loo_net = cross_val_score(Net, Resistome, Sus_zero_Res_uno, cv=loo, n_jobs=-1)
-#results_loo_net = cross_val_predict(Net, Resistome, Sus_zero_Res_uno, cv=loo, n_jobs=-1)
-#scores_cv5_net = cross_val_score(Net, Resistome, Sus_zero_Res_uno, cv=5, n_jobs=-1)
-#results_cv5_net = cross_val_predict(Net, Resistome, Sus_zero_Res_uno, cv=5, n_jobs=-1)
+print("\nAdaBoost classifier:")
+Ada = AdaBoostClassifier(n_estimators=100, random_state=0)
+Ada_loo_scores = loo(Ada, Resistome, Phenotype, plot=True)
+Ada_cv5_scores = cv5(Ada, Resistome, Phenotype, plot=True)
+
+print("\nLog regression classifier:")
+Log = LogisticRegression(random_state=20, penalty="elasticnet", solver = "saga", l1_ratio=0.5)
+Net_loo_scores = loo(Log, Resistome, Phenotype, plot=True, linear=True)
+Net_cv5_scores = cv5(Log, Resistome, Phenotype, plot=True, linear=True)
