d01ac64e32a563743127258321339b72e3612e62
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 21:32:21 +0000

    Adding numeric_owner as keyword arguement

--- CAMISIM/scripts/Archive/archive.py
+++ CAMISIM/scripts/Archive/archive.py
@@ -148,7 +148,7 @@ class Archive(Compress):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(read_handler, output_directory)
